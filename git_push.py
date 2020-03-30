919b5a66fd93a89f3578becdb83704d812f1e95d
Bulgakov Anton <bulgakov.anton.a@gmail.com>
2020-03-30 22:55:36 +0300

    Save changes by cron job

---
+++ git_push.py
@@ -0,0 +1,11 @@
+import os
+import requests
+
+git_add_result_code = os.system("git add .")
+print('Changes added with exit code: ' + str(git_add_result_code))
+
+git_commit_result_code = os.system("git commit -m 'Save changes by cron job'")
+print('Changes committed with exit code: ' + str(git_commit_result_code))
+
+git_push_result_code = os.system("git push origin maste")
+print('Changes committed with exit code: ' + str(git_push_result_code))

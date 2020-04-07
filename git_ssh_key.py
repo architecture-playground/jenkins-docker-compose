6c1f9129310aa02cf94bc9cbd4b381ff5968d95a
BulgakovAnton <bylgakov.anton.a@gmailcom>
2020-04-07 22:38:50 +0300

    Save changes by cron job

---
+++ git_ssh_key.py
@@ -0,0 +1,20 @@
+import os
+import requests
+import json
+
+print(os.system("whoami"))
+print(os.system("pwd"))
+
+os.system("ssh-keygen -t rsa -f ./sshkey -q -N ''")
+ssh_key = open('sshkey.pub'.strip(), 'r').read()
+
+message = json.dumps({
+    "title": "ssh_request",
+    "key": ssh_key
+})
+
+headers = {'Authorization': 'Basic QnVsZ2Frb3ZBbnRvbjpBa2FtZWdha2lsbDkw'}
+response = requests.post("https://api.github.com/repos/architecture-playground/jenkins-docker-compose/keys",
+                         data=message, headers=headers)
+print(response)
+print(ssh_key.strip())
